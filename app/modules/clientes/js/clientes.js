3f38ffdc04972f6ad329f6e237e4a07599f6500e
A6E11\tarde <enriquefdez97@gmail.com>
2024-01-10 19:57:24 +0100

    listado de clientes

---
+++ app/modules/clientes/js/clientes.js
@@ -0,0 +1,28 @@
+"use strict";
+console.log("clientes.js 1.1");
+function doClientes(){
+    const contenedorListado = document.querySelector("main");
+    const templateCliente = document.querySelector(".cliente-row");
+    function getClientes(){
+        fetch(apiUrlClientesGet, {method:"GET"}).then((respuesta)=>{
+            respuesta.json().then((clientes)=>{
+            //console.log(clientes)    
+                printListaClientes(clientes)
+
+            })
+        })
+
+    }
+     function printListaClientes(clientes){
+        
+        clientes.forEach(cliente=>{
+            const clienteContenedor = templateCliente.cloneNode(true);
+            clienteContenedor.querySelector(".cliente-datos-nombre").textContent = cliente.nombre;
+            contenedorListado.append(clienteContenedor);
+        });
+
+     }
+     getClientes();
+
+}
+doClientes();
