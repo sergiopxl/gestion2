2f51da9be9e780f74cf98195e5da5b776fa18b0e
unknown <lucian.bouysede@gmail.com>
2024-01-16 19:10:17 +0100

    link contactos

--- app/modules/clientes/js/clientes.js
+++ app/modules/clientes/js/clientes.js
@@ -14,10 +14,33 @@ function doClientes(){
 
     }
      function printListaClientes(clientes){
+        contenedorListado.innerHTML = "";
         
         clientes.forEach(cliente=>{
             const clienteContenedor = templateCliente.cloneNode(true);
+            clienteContenedor.classList.remove("hidden");
+            const clientesContactosContenedor = clienteContenedor.querySelector(".cliente-row-contactos");
+            const templateContacto = clientesContactosContenedor.querySelector(".contactos-contacto");
+
+            //contenido)
             clienteContenedor.querySelector(".cliente-datos-nombre").textContent = cliente.nombre;
+            clienteContenedor.querySelector(".cliente-datos-cif").txtContent = cliente.cif;
+            clienteContenedor.querySelector(".cliente-datos-nombre").textContent = cliente.telefono;
+            clienteContenedor.querySelector(".cliente-datos-nombre").textContent = cliente.direccion;
+
+            //CLONADO DE CONTACTOS
+            cliente.contactos.forEach((contacto,index)=>{
+                console.log(cliente.contactos[0])
+                const contactoContenedor = templateContacto.cloneNode(true);
+                contactoContenedor.classList.remove("hidden");
+                //contenido de cada contacto
+                contactoContenedor.querySelector(".contacto-nombre").textContent = contacto.nombre + " "+contacto.apellido1 + " "+contacto.apellido2;
+                contactoContenedor.querySelector(".contacto-tlf").textContent = contacto.telefono1;
+                contactoContenedor.querySelector(".contacto-email").textContent = contacto.email1;
+                clientesContactosContenedor.append(contactoContenedor)
+            })
+            
+            
             contenedorListado.append(clienteContenedor);
         });
 
