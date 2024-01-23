1f22fdaa484bd96cad083fa5a952bb1de4fa1c85
A6E11\tarde <enriquefdez97@gmail.com>
2024-01-23 21:28:33 +0100

    creacion y asociacion de valores clientes post

---
+++ api/clientes_post.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, X-User-ID,X-Token, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: POST");
+include("conn/conexion.php");
+
+$nombre = $_POST["input-cliente-nombre"];
+$cif = $_POST["input-cliente-cif"];
+$telefono = $_POST["input-cliente-tlf"];
+$direccion = $_POST["input-cliente-direccion"];
+$idSector = $_POST["select-cliente-sector"];
+
+
+
+$sqlClientesNew = "INSERT INTO `clientes_tb`(`id`, `nombre`, `cif`, `direccion`, `cp`, `provincia`, `poblacion`, `telefono`, `web`, `descripcion`, `cuenta`, `activo`, `id_sector`, `id_origen`, `id_servicio`, `id_usuario`)
+VALUES
+('',$nombre,$cif,$direccion,'','','',$telefono,'','','',1,$idSector,1,1,1)"
+
+$respuesta = mysqli_query($conn,$sqlClientesNew);
+if($respuesta){
+    $mensaje = "Creado correctamente";
+
+}else{
+    $mensaje = "Tienes un problema";    
+}
+echo json_encode($mensaje);
