56336d9c6882ee7ea6abc4bf2e2966a8dd328d9a
A6E11\tarde <enriquefdez97@gmail.com>
2024-01-30 15:26:15 +0100

    commit resultado merge

--- api/clientes_contactos_post.php
+++ api/clientes_contactos_post.php
@@ -11,10 +11,10 @@ $apellido1 = $_POST["input-contacto-apellido1"];
 $apellido2 = $_POST["input-contacto-apellido2"];
 $telefono = $_POST["input-contacto-telefono"];
 $email = $_POST["input-contacto-email"];
-$idCliente = $_POST["input-cliente-id"];
+$id_Cliente = $_POST["input-contacto-cliente-id"];
 
 $sqlContactoInsert = "INSERT INTO `clientes_contactos_tb`(`nombre`, `apellido1`, `apellido2`, `telefono1`, `email1`, `id_cliente`)
-VALUES ('$nombre', '$apellido1', '$apellido2', '$telefono1', '$email1', $id_cliente);
+VALUES ('$nombre', '$apellido1', '$apellido2', '$telefono', '$email', '$id_Cliente')";
 
 $respuesta = mysqli_query($conn,$sqlContactoInsert);
 if($respuesta){
--- app/assets/js/api_roots.js
+++ app/assets/js/api_roots.js
@@ -7,3 +7,5 @@ const apiUrlFacturasGet = "http://localhost/gestion2/api/facturas_get.php";
 const apiUrlComunicacionesGet = "http://localhost/gestion2/api/comunicaciones_get.php";
 const apiUrlClientesUpdate = "http://localhost/gestion2/api/clientes_update.php"
 const apiUrlClientesPost = "http://localhost/gestion2/api/clientes_post.php"
+const apiUrlContactosUpdate="http://localhost/gestion2/api/clientes_contactos_update.php"
+const apiUrlContactosPost="http://localhost/gestion2/api/clientes_contactos_post.php"
