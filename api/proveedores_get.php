80f14a8a20c8b94ec0e924ad52eb69d89f996840
A6E11\tarde <enriquefdez97@gmail.com>
2024-01-30 18:37:01 +0100

    api proveedores get

---
+++ api/proveedores_get.php
@@ -0,0 +1,52 @@
+<?php
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, X-User-ID,X-Token, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: GET");
+include("conn/conexion.php");
+
+$inicio = $_GET["inicio"];
+$porPagina = $_GET["porpagina"];
+$condicion = " WHERE 1 ";
+$limite = " LIMIT $inicio, $porPagina ";
+$respuesta=[];
+if(isset($_GET["buscar"])){
+    $buscar = $_GET["buscar"];
+    $condicion = " WHERE proveedores_tb.nombre LIKE '%".$buscar."%' OR proveedores_tb.cif LIKE '%".$buscar."%' ";
+}
+
+$sqlProveedores="SELECT proveedores_tb.*,proveedores_servicios_tb.name AS sevicio FROM proveedores_tb  
+LEFT JOIN proveedores_servicios_tb ON proveedores_tb.id_servicio = proveedores_servicios_tb.id $condicion ORDER BY proveedores_tb.id DESC $limite  ";
+$respuestaProveedores = mysqli_query($conn,$sqlProveedores);
+
+$sqlNumeroRegistros = "SELECT COUNT(*) AS numero_registros FROM proveedores_tb WHERE 1";
+$respuestaNumeroRegistros = mysqli_query($conn,$sqlNumeroRegistros);
+
+$fila =mysqli_fetch_assoc($respuestaNumeroRegistros);
+$respuesta["numero_registros"] = $fila['numero_registros'];
+
+$proveedores=[];
+while($proveedor = mysqli_fetch_assoc($respuestaProveedores)){
+
+    $sqlContactos= "SELECT * FROM proveedores_contactos_tb WHERE id_proveedor = " . $proveedor["id"];
+    $resultadoContactos= mysqli_query($conn,$sqlContactos);
+    $contactos = [];
+    while($contacto = mysqli_fetch_assoc($resultadoContactos)){
+        $contactos[] = $contacto;
+    }
+    $proveedor["contactos"] = $contactos;
+    $proveedores[] = $proveedor;
+
+    //?inicio=1&porpagina=10
+   
+
+}
+ 
+$respuesta["proveedores"]= $proveedores;
+
+echo json_encode($respuesta);
+
+
+?>
+
