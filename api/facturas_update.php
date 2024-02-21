b2413b2e867fdc7471002029f17fb928a7c336e3
A6E11\tarde <enriquefdez97@gmail.com>
2024-02-21 16:00:46 +0100

    front facturas

---
+++ api/facturas_update.php
@@ -0,0 +1,7 @@
+<?php
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, X-User-ID,X-Token, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: POST");
+include("conn/conexion.php");
