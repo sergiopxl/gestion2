213db962fadfc5ac25eec7695db786ac966d9765
Quique9723 <enriquefdez97@gmail.com>
2024-02-22 01:04:29 +0100

    clientes comentado entero

--- api/clientes_get.php
+++ api/clientes_get.php
@@ -1,60 +1,71 @@
 <?php
+// Establece el tipo de contenido como JSON para la respuesta.
 header('Content-Type: application/json');
+// Establece los encabezados para permitir el acceso desde cualquier origen.
 header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, X-User-ID,X-Token, Accept, Accept-Encoding");
+// Establece los encabezados permitidos.
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, X-User-ID, X-Token, Accept, Accept-Encoding");
+// Establece los métodos permitidos.
 header("Access-Control-Allow-Methods: GET");
+// Incluye el archivo de conexión a la base de datos.
 include("conn/conexion.php");
 
-
-
+// Define la condición inicial para la consulta.
 $condicion = " WHERE activo = 1 ";
 $limite = "";
+
+// Verifica si se proporciona un valor para el inicio de la paginación.
 if (isset($_GET["inicio"])){
     $inicio = $_GET["inicio"];
- 
-}else {
+} else {
     $inicio = 0;
 }
 
+// Verifica si se proporciona un término de búsqueda.
 if(isset($_GET["buscar"])){
-    
     $buscar = $_GET["buscar"];
+    // Modifica la condición de la consulta para incluir el término de búsqueda.
     $condicion = " WHERE activo = 1 AND (clientes_tb.nombre LIKE '%$buscar%' OR clientes_tb.cif LIKE '%$buscar%' ) ";
 }
+
+// Verifica si se proporciona un valor para el número de resultados por página.
 if (isset($_GET["porpagina"])) {
     $porPagina = $_GET["porpagina"];
-
-}else {
-    $porPagina = 30;
+} else {
+    $porPagina = 30; // Establece un valor por defecto para el número de resultados por página.
 }
+// Construye la parte de la consulta que limita los resultados.
 $limite = " LIMIT $inicio, $porPagina ";
 
 $respuesta = [];
 
+// Realiza una consulta para obtener el número total de registros.
 $sqlNumeroRegistros = "SELECT COUNT(*) AS numero_registros FROM clientes_tb WHERE activo = 1";
 $respuestaNumeroRegistros = mysqli_query($conn,$sqlNumeroRegistros);
 
+// Obtiene el número total de registros y lo agrega a la respuesta.
 $fila =mysqli_fetch_assoc($respuestaNumeroRegistros);
 $respuesta["numero_registros"] = $fila['numero_registros'];
 
-
-
+// Realiza la consulta para obtener los clientes con sus contactos.
 $sqlClientes = "SELECT clientes_tb.*, clientes_sectores_tb.nombre AS sector FROM clientes_tb 
  LEFT JOIN clientes_sectores_tb ON clientes_tb.id_sector = clientes_sectores_tb.id $condicion ORDER BY clientes_tb.id DESC $limite  ";
 $resultadoClientes = mysqli_query($conn,$sqlClientes);
-//var_dump($resultadoClientes);
+
 $clientes = [];
+// Itera sobre los resultados de la consulta de clientes.
 while($cliente = mysqli_fetch_assoc($resultadoClientes)){
+    // Para cada cliente, consulta sus contactos.
     $sqlContactos= "SELECT * FROM clientes_contactos_tb WHERE id_cliente = " . $cliente["id"];
     $resultadoContactos= mysqli_query($conn,$sqlContactos);
     $contactos = [];
+    // Itera sobre los resultados de la consulta de contactos.
     while($contacto = mysqli_fetch_assoc($resultadoContactos)){
-        $contactos[] = $contacto;
+        $contactos[] = $contacto; // Agrega el contacto al array de contactos.
     }
-    $cliente["contactos"]= $contactos;
-    $clientes[] = $cliente;
+    $cliente["contactos"]= $contactos; // Agrega el array de contactos al cliente.
+    $clientes[] = $cliente; // Agrega el cliente al array de clientes.
 }
-$respuesta["clientes"] = $clientes;
-echo json_encode($respuesta);
+$respuesta["clientes"] = $clientes; // Agrega el array de clientes a la respuesta.
+echo json_encode($respuesta); // Devuelve la respuesta en formato JSON.
 ?>
--- api/clientes_update.php
+++ api/clientes_update.php
@@ -1,11 +1,17 @@
 <?php
+// Establece el encabezado para indicar que el contenido es JSON
 header('Content-Type: application/json');
+// Permite solicitudes de cualquier origen (CORS)
 header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Credentials: true");
+// Define los encabezados permitidos en la solicitud
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, X-User-ID,X-Token, Accept, Accept-Encoding");
+// Define los métodos HTTP permitidos para esta solicitud
 header("Access-Control-Allow-Methods: POST");
+
+// Incluye el archivo de conexión a la base de datos
 include("conn/conexion.php");
 
+// Obtiene los datos del formulario enviado por POST
 $nombre = $_POST["input-cliente-nombre"];
 $cif = $_POST["input-cliente-cif"];
 $telefono = $_POST["input-cliente-tlf"];
@@ -13,6 +19,7 @@ $direccion = $_POST["input-cliente-direccion"];
 $idSector = $_POST["select-cliente-sector"];
 $idCliente = $_POST["input-cliente-id"];
 
+// Query SQL para actualizar el cliente en la base de datos
 $sqlClienteUpdate = "UPDATE clientes_tb SET
 nombre = '$nombre',
 cif = '$cif',
@@ -20,11 +27,16 @@ telefono = $telefono,
 direccion = '$direccion',
 id_sector = '$idSector' WHERE id = $idCliente";
 
+// Ejecuta la consulta SQL
 $respuesta = mysqli_query($conn,$sqlClienteUpdate);
-if($respuesta){
-    $mensaje = "Registro actualizado correctamente";
 
-}else{
-    $mensaje = "Tienes un problema";    
+// Verifica si la consulta se ejecutó correctamente
+if($respuesta){
+    $mensaje = "Registro actualizado correctamente"; // Mensaje de éxito
+} else {
+    $mensaje = "Tienes un problema"; // Mensaje de error
 }
+
+// Convierte el mensaje a formato JSON y lo imprime
 echo json_encode($mensaje);
+?>
